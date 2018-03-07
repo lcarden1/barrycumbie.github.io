9453173325ba9b6bf6844371116b30047f9583f0
lcarden1 <37086909+lcarden1@users.noreply.github.com>
2018-03-07 15:17:45 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//my js file
+
+$(document).ready(function() {
+	
+	//create a loop to print out all elements in the DOM
+	//1. create my loop structure. How to figure out the length ie the number of iterations in my loop. 
+	//2. how do I access an element from my page..ll elements?
+	//3. How do I want to print them out? Start on the console. 
+	
+	var allDOMElements = document; //here's a start.. what does this do?
+	console.log(allDOMElements);
+	
+	
+	$("#close-btn").click(function() {
+		
+	$(".buttons").click(function() {
+		
+	//create an element with jscript and/or jquery...
+	
+	function printDOM() {
+		console.log(document.getElementsByTagName('body')[0].innerHTML);
+	
+	
+		
+	
+	document.var all = document.getElementsByTagName("*");
+	
+	
+	
+	
+		
+	}
+	}
+
+	
+	
+	
+	
+	}
